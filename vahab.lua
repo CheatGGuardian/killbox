686736c063153af2229cda9c732699e8a0f24fb5
CheatGGuardian <45544901+CheatGGuardian@users.noreply.github.com>
2018-12-06 12:56:55 +0330

    Add files via upload

---
+++ vahab.lua
@@ -0,0 +1,20 @@
+print('@MrTermux')
+gg.alert('Salam Vahab Jon\n\nBe Cheat Khosh Omadi')
+menu = gg.multiChoice({'CheatBosMode','Exit'},nil,'Cheat For VahaB')
+if menu == nil then else
+if menu[1] == true then goto CBM end
+end
+goto credit
+
+::CBM::
+gg.clearResults()
+gg.searchNumber('1036831949;1075838976', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('1036831949 ', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(5000)
+print('Replaced: ', gg.editAll('1130030938', gg.TYPE_DWORD))
+gg.clearResults()
+gg.toast('چیت باس مود فعال شد \nوهاب جون')
+goto credit
+
+print('@MrTermux')
+::credit::
